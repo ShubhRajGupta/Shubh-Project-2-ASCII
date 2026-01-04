82ec140bebabd58d377e8f43960c5f97965c2888
Shubh <shubhrajgupta.1@gmail.com>
2026-01-05 00:46:20 +0530

    first commit

---
+++ stephen-code.py
@@ -0,0 +1,545 @@
+def print_art():
+    # Line 1: spaces then #'s
+    for i in range(33):
+        print(' ', end='')
+    for i in range(16):
+        print('#', end='')
+    for i in range(31):
+        print(' ', end='')
+    print('l')
+    
+    # Line 2
+    for i in range(26):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(29):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(23):
+        print(' ', end='')
+    print('l')
+    
+    # Line 3
+    for i in range(21):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(39):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(18):
+        print(' ', end='')
+    print('l')
+    
+    # Line 4
+    for i in range(18):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(44):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(16):
+        print(' ', end='')
+    print('l')
+    
+    # Line 5
+    for i in range(16):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(48):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(14):
+        print(' ', end='')
+    print('l')
+    
+    # Line 6
+    for i in range(15):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(50):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(13):
+        print(' ', end='')
+    print('l')
+    
+    # Line 7
+    for i in range(14):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(52):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(12):
+        print(' ', end='')
+    print('l')
+    
+    # Line 8: with .#=. ..
+    for i in range(14):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(12):
+        print(' ', end='')
+    print('.#=. ..', end='')
+    for i in range(34):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(11):
+        print(' ', end='')
+    print('l')
+    
+    # Line 9
+    for i in range(14):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(11):
+        print(' ', end='')
+    print('#-***+--=-.       .:.', end='')
+    for i in range(22):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(10):
+        print(' ', end='')
+    print('l')
+    
+    # Line 10
+    for i in range(14):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(10):
+        print(' ', end='')
+    chars = '#-+=----==++@@@@@@@@@@*........--:'
+    for c in chars:
+        print(c, end='')
+    for i in range(9):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(11):
+        print(' ', end='')
+    print('l')
+    
+    # Line 11
+    for i in range(15):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(8):
+        print(' ', end='')
+    chars = '#-//++++++++\\\\@@@@@@@@@//+++  ++++\\\\'
+    for c in chars:
+        print(c, end='')
+    for i in range(11):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(10):
+        print(' ', end='')
+    print('l')
+    
+    # Line 12
+    for i in range(16):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(7):
+        print(' ', end='')
+    chars = '#\\\\.+*--=(  ).//&&&&\\\\\\=+(  )+--*//.+'
+    for c in chars:
+        print(c, end='')
+    for i in range(7):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(11):
+        print(' ', end='')
+    print('l')
+    
+    # Line 13
+    for i in range(16):
+        print(' ', end='')
+    chars = '# +++'
+    for c in chars:
+        print(c, end='')
+    for i in range(3):
+        print(' ', end='')
+    chars = '#\\\\=*********//&&&&&&&\\*********//=*'
+    for c in chars:
+        print(c, end='')
+    for i in range(7):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(12):
+        print(' ', end='')
+    print('l')
+    
+    # Line 14
+    for i in range(17):
+        print(' ', end='')
+    chars = '#.+=-  #*+\\\\++++++//&&&&&&&&&\\++++++++//-+*='
+    for c in chars:
+        print(c, end='')
+    for i in range(6):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(12):
+        print(' ', end='')
+    print('l')
+    
+    # Line 15
+    for i in range(18):
+        print(' ', end='')
+    chars = '# -= .#++++++++=+++^^^^^^^^^^^+++++++++++++ :- #'
+    for c in chars:
+        print(c, end='')
+    for i in range(14):
+        print(' ', end='')
+    print('l')
+    
+    # Line 16
+    for i in range(18):
+        print(' ', end='')
+    chars = '#    ..#:*=+-$^^^^^^^^^^^^^^^^^^^^^^+++++'
+    for c in chars:
+        print(c, end='')
+    for i in range(8):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(12):
+        print(' ', end='')
+    print('l')
+    
+    # Line 17
+    for i in range(17):
+        print(' ', end='')
+    chars = '#    . .:#      .:::::-:::::.         :..'
+    for c in chars:
+        print(c, end='')
+    for i in range(6):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(15):
+        print(' ', end='')
+    print('l')
+    
+    # Line 18
+    for i in range(18):
+        print(' ', end='')
+    chars = '#      #      #.....:::....::.#        .'
+    for c in chars:
+        print(c, end='')
+    for i in range(7):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(14):
+        print(' ', end='')
+    print('l')
+    
+    # Line 19
+    for i in range(20):
+        print(' ', end='')
+    chars = '#  #       #...  ..............#'
+    for c in chars:
+        print(c, end='')
+    for i in range(11):
+        print(' ', end='')
+    print('#', end='')
+    for i in range(16):
+        print(' ', end='')
+    print('l')
+    
+    # Line 20
+    for i in range(17):
+        print(' ', end='')
+    for i in range(6):
+        print('_', end='')
+    print('#', end='')
+    for i in range(8):
+        print(' ', end='')
+    for i in range(18):
+        print('#', end='')
+    for i in range(9):
+        print(' ', end='')
+    for i in range(2):
+        print('_', end='')
+    for i in range(2):
+        print('#', end='')
+    for i in range(8):
+        print('_', end='')
+    for i in range(9):
+        print(' ', end='')
+    print('l')
+    
+    # Line 21
+    for i in range(16):
+        print(' ', end='')
+    for i in range(9):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(4):
+        print('#', end='')
+    for i in range(27):
+        print(' ', end='')
+    for i in range(2):
+        print('#', end='')
+    print(' ', end='')
+    for i in range(14):
+        print('\\', end='')
+    for i in range(6):
+        print(' ', end='')
+    print('l')
+    
+    # Line 22
+    for i in range(4):
+        print(' ', end='')
+    for i in range(11):
+        print('0', end='')
+    for i in range(9):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(12):
+        print('#', end='')
+    for i in range(8):
+        print(' ', end='')
+    for i in range(14):
+        print('#', end='')
+    print(' ', end='')
+    for i in range(15):
+        print('\\', end='')
+    for i in range(5):
+        print(' ', end='')
+    print('l')
+    
+    # Line 23
+    for i in range(4):
+        print(' ', end='')
+    for i in range(9):
+        print('0', end='')
+    print(' ', end='')
+    for i in range(11):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(33):
+        print('#', end='')
+    print('  ', end='')
+    for i in range(16):
+        print('\\', end='')
+    for i in range(4):
+        print(' ', end='')
+    print('l')
+    
+    # Line 24
+    for i in range(3):
+        print(' ', end='')
+    for i in range(8):
+        print('0', end='')
+    for i in range(14):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(32):
+        print('#', end='')
+    for i in range(19):
+        print('\\', end='')
+    for i in range(3):
+        print(' ', end='')
+    print('l')
+    
+    # Line 25
+    for i in range(2):
+        print(' ', end='')
+    for i in range(8):
+        print('0', end='')
+    for i in range(15):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(33):
+        print('#', end='')
+    for i in range(18):
+        print('\\', end='')
+    print('  ', end='')
+    print('1')
+    
+    # Line 26
+    for i in range(9):
+        print('0', end='')
+    for i in range(17):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(33):
+        print('#', end='')
+    print(' ', end='')
+    for i in range(17):
+        print('/', end='')
+    print('  ', end='')
+    print('l')
+    
+    # Line 27
+    for i in range(9):
+        print('0', end='')
+    for i in range(17):
+        print('\\', end='')
+    print(' ', end='')
+    for i in range(32):
+        print('#', end='')
+    print(' ', end='')
+    for i in range(16):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(3):
+        print('0', end='')
+    print('l')
+    
+    # Line 28
+    for i in range(9):
+        print('0', end='')
+    print(' ', end='')
+    for i in range(18):
+        print('\\', end='')
+    print(' ', end='')
+    for i in range(30):
+        print('#', end='')
+    print(' ', end='')
+    for i in range(17):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(3):
+        print('0', end='')
+    print('l')
+    
+    # Line 29
+    for i in range(10):
+        print('0', end='')
+    print(' ', end='')
+    for i in range(16):
+        print('\\', end='')
+    print(' ', end='')
+    for i in range(29):
+        print('#', end='')
+    print(' ', end='')
+    for i in range(16):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(5):
+        print('0', end='')
+    print('l')
+    
+    # Line 30
+    for i in range(11):
+        print('0', end='')
+    print(' ', end='')
+    for i in range(16):
+        print('\\', end='')
+    print(' ', end='')
+    for i in range(29):
+        print('#', end='')
+    print(' ', end='')
+    for i in range(15):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(6):
+        print('0', end='')
+    print('l')
+    
+    # Line 31
+    for i in range(12):
+        print('0', end='')
+    print(' ', end='')
+    for i in range(17):
+        print('\\', end='')
+    print(' ', end='')
+    for i in range(28):
+        print('#', end='')
+    print(' ', end='')
+    for i in range(15):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(6):
+        print('0', end='')
+    print('l')
+    
+    # Line 32
+    for i in range(13):
+        print('0', end='')
+    print(' ', end='')
+    for i in range(16):
+        print('\\', end='')
+    print(' ', end='')
+    for i in range(27):
+        print('#', end='')
+    print(' ', end='')
+    for i in range(14):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(8):
+        print('0', end='')
+    print('l')
+    
+    # Line 33
+    for i in range(14):
+        print('0', end='')
+    print(' ', end='')
+    for i in range(16):
+        print('\\', end='')
+    print('  ', end='')
+    for i in range(25):
+        print('#', end='')
+    print('0 ', end='')
+    for i in range(13):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(9):
+        print('0', end='')
+    print('l')
+    
+    # Line 34
+    for i in range(15):
+        print('0', end='')
+    print(' ', end='')
+    for i in range(17):
+        print('\\', end='')
+    for i in range(3):
+        print(' ', end='')
+    for i in range(22):
+        print('#', end='')
+    print('* ', end='')
+    for i in range(13):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(10):
+        print('0', end='')
+    print('l')
+    
+    # Line 35
+    for i in range(15):
+        print('0', end='')
+    print(' ', end='')
+    for i in range(18):
+        print('\\', end='')
+    for i in range(4):
+        print(' ', end='')
+    for i in range(19):
+        print('#', end='')
+    print(' ', end='')
+    for i in range(13):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(12):
+        print('0', end='')
+    print('l')
+    
+    # Line 36
+    for i in range(16):
+        print('0', end='')
+    print(' ', end='')
+    for i in range(18):
+        print('\\', end='')
+    for i in range(4):
+        print(' ', end='')
+    print('*', end='')
+    for i in range(11):
+        print('#', end='')
+    for i in range(4):
+        print(' ', end='')
+    for i in range(14):
+        print('/', end='')
+    print(' ', end='')
+    for i in range(14):
+        print('0', end='')
+    print('l')
+
+print_art()
